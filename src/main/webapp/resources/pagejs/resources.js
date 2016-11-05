a09c833783d8994bee832bb902383de2930e58d8
pwy <pengweiyuang@airag.cn>
2016-11-05 17:41:23 +0800

    add some resource such as js css and simple page.

---
+++ src/main/webapp/resources/pagejs/resources.js
@@ -0,0 +1,47 @@
+/**
+ * Created by OPtilex on 2016/11/4.
+ */
+var resource=function () {
+    var _self=this;
+    _self.init=function () {
+        $("#change").bind("click",function () {
+            data.title="dddsljflsjflsjf";
+            var html = template('test', data);
+            document.getElementById('content').innerHTML = html;
+        });
+    }
+
+    _self.generateTable=function(data) {
+        var html = template('test', data);
+        document.getElementById('content').innerHTML = html;
+    }
+    
+    _self.loadResourceData=function () {
+        $.ajax({
+            type: "get",
+            dataType: "json",
+            url: "resources",
+            data: {},
+            success: function(data) {
+                if(data.code===0){
+                    toastr.success('操作成功');
+                    _self.generateTable(data)
+                    var content= generatePageing(data.data.totalRow,null);
+                    alert(content);
+                }else{
+                    toastr.error('操作失败');
+                }
+            },
+            error: function(err) {
+                toastr.error('操作失败');
+            }
+        });
+    }
+}
+
+var res;
+$(document).ready(function () {
+    res=new resource();
+    res.init();
+    res.loadResourceData();
+});
---
+++ src/main/webapp/resources/pagejs/utils.js
@@ -0,0 +1,54 @@
+/**
+ * Created by OPtilex on 2016/11/4.
+ */
+var defaultLength = 10;//默认分页阑珊
+
+var paging=function (total,callback) {
+    var _self=this;
+    _self.init=function () {
+        if(total<defaultLength)return;
+        _self.generateHtml(1);
+    }
+    
+    _self.generateHtml=function (index) {
+        var content = "<li class=\"disabled\"><a href=\"javascript:void(0);\"> &laquo;</a></li>";
+        for (var i = index; i <= index*defaultLength; i++) {
+            if(i==index){
+                content += "<li><a href=\"javascript:void(0);\" class='active'>" + i + "</a></li>";
+            }else
+                content += "<li><a href=\"javascript:void(0);\">" + i + "</a></li>";
+        }
+        content += "<li><a href=\"javascript:void(0);\" onclick=''>&raquo;</a></li>";
+        $(".pagination").empty();
+        $(content).appendTo($(".pagination"));
+    }
+    
+    _self.nextPage=function () {
+        
+    }
+    
+}
+
+
+var defaultLength = 10;//默认分页阑珊
+var TOTAL;
+function generatePageing(total, callBack) {
+    if(total<10)return;
+    TOTAL=total;
+    var conent = "<li class=\"disabled\"><a href=\"javascript:void(0);\"> &laquo;</a></li>";
+    if(total >10){
+        total=10;
+    }
+    for (var i = 1; i <= total; i++) {
+        if(i==1){
+            conent += "<li><a href=\"javascript:void(0);\" class='active'>" + i + "</a></li>";
+        }else
+            conent += "<li><a href=\"javascript:void(0);\">" + i + "</a></li>";
+    }
+    conent += "<li><a href=\"javascript:void(0);\" onclick=''>&raquo;</a></li>";
+    $(conent).appendTo($(".pagination"));
+}
+
+function next(number) {
+
+}
